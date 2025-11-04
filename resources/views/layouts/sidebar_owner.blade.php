6837b80b4b49e283a30e6b33671c7df6423b6e98
Made Aditya <madeaditya202@gmail.com>
2025-11-04 16:48:10 +0700

    tampilan perbaikan

--- resources/views/layouts/sidebar_owner.blade.php
+++ resources/views/layouts/sidebar_owner.blade.php
@@ -39,6 +39,11 @@
                     </svg>
                     <span>Data Pelanggan</span>
                 </a>
+                <a href="{{ route('bahan-baku.index') }}"
+        class="nav-item {{ request()->routeIs('bahan-baku.*') ? 'active' : '' }}">
+        <i data-feather="package"></i>
+        <span>Bahan Baku</span>
+        </a>
                 <a href="#" class="nav-item">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z"/>
@@ -76,6 +81,7 @@
                     <span>Keluar</span>
                 </button>
             </div>
+
         </aside>
         
         <script>
