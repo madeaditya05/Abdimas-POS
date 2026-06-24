159124e6c861422c7c5fcee9f7d616f848bcb840
agung <madeaditya202@gmail.com>
2026-06-24 07:37:14 +0700

    fix button

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') - Pasta Nafisa</title>
     {{-- CSS global --}}
-    <link rel="stylesheet" href="{{ asset('assets/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/styles.css') }}?v={{ filemtime(public_path('assets/styles.css')) }}">
 
 
     {{-- Slot untuk CSS halaman (product.css, DataTables, dll) --}}
